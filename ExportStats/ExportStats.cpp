df91440719c52588cff1442e2d6c529cbe26b0ca
Pool Noodle <iohcalex@gmail.com>
2025-06-02 14:59:20 -0400

    LOCAL WORKING VERSION, fixed DFH Stadium and Farmstead error messages

--- ExportStats/ExportStats.cpp
+++ ExportStats/ExportStats.cpp
@@ -13,135 +13,6 @@ using json = nlohmann::json;
 #include "bakkesmod/wrappers/GameObject/Stats/StatEventWrapper.h"
 
 std::unordered_map<std::string, std::string> mapNameLookup = {
-    // Standard Maps
-    {"stadium_p", "DFH Stadium"},
-    {"stadium_day_p", "DFH Stadium (Day)"},
-    {"stadium_snowy_p", "DFH Stadium (Snowy)"},
-    {"stadium_stormy_p", "DFH Stadium (Stormy)"},
-    {"stadium_circuit_p", "DFH Stadium (Circuit)"},
-
-    {"cheetah_p", "Utopia Coliseum"},
-    {"cheetah_day_p", "Utopia Coliseum (Day)"},
-    {"cheetah_dusk_p", "Utopia Coliseum (Dusk)"},
-    {"cheetah_snowy_p", "Utopia Coliseum (Snowy)"},
-    {"cheetah_gilded_p", "Utopia Coliseum (Gilded)"},
-
-    {"urban_p", "Urban Central"},
-    {"urban_night_p", "Urban Central (Night)"},
-    {"urban_dawn_p", "Urban Central (Dawn)"},
-    {"urban_rainy_p", "Urban Central (Rainy)"},
-    {"urban_haunted_p", "Urban Central (Haunted)"},
-
-    {"park_p", "Beckwith Park"},
-    {"park_night_p", "Beckwith Park (Night)"},
-    {"park_stormy_p", "Beckwith Park (Stormy)"},
-    {"park_midnight_p", "Beckwith Park (Midnight)"},
-    {"park_snowy_p", "Beckwith Park (Snowy)"},
-
-    {"trainstation_p", "Mannfield"},
-    {"trainstation_day_p", "Mannfield (Day)"},
-    {"trainstation_night_p", "Mannfield (Night)"},
-    {"trainstation_dusk_p", "Mannfield (Dusk)"},
-    {"trainstation_snowy_p", "Mannfield (Snowy)"},
-    {"trainstation_stormy_p", "Mannfield (Stormy)"},
-
-    {"wasteland_p", "Wasteland"},
-    {"wasteland_night_p", "Wasteland (Night)"},
-    {"wasteland_day_p", "Wasteland (Day)"},
-    {"wasteland_pitched_p", "Wasteland (Pitched)"},
-
-    {"arc_p", "Neo Tokyo"},
-    {"arc_rainy_p", "Neo Tokyo (Rainy)"},
-    {"arc_comic_p", "Neo Tokyo (Comic)"},
-    {"arc_hacked_p", "Neo Tokyo (Hacked)"},
-
-    {"aquadome_p", "AquaDome"},
-    {"aquadome_day_p", "AquaDome (Day)"},
-
-    {"championsfield_p", "Champions Field"},
-    {"championsfield_day_p", "Champions Field (Day)"},
-    {"championsfield_night_p", "Champions Field (Night)"},
-    {"championsfield_nfl_p", "Champions Field (NFL)"},
-    {"championsfield_nikefc_p", "Champions Field (Nike FC)"},
-
-    {"farm_p", "Farmstead"},
-    {"farm_night_p", "Farmstead (Night)"},
-    {"farm_dusk_p", "Farmstead (Dusk)"},
-    {"farm_pitched_p", "Farmstead (Pitched)"},
-    {"farm_spooky_p", "Farmstead (Spooky)"},
-    {"farm_upside_down_p", "Farmstead (The Upside Down)"},
-
-    {"temple_p", "Forbidden Temple"},
-    {"temple_day_p", "Forbidden Temple (Day)"},
-    {"temple_fireice_p", "Forbidden Temple (Fire & Ice)"},
-
-    {"saltyshores_p", "Salty Shores"},
-    {"saltyshores_night_p", "Salty Shores (Night)"},
-    {"saltyshores_fest_p", "Salty Shores (Salty Fest)"},
-
-    {"neonfields_p", "Neon Fields"},
-    {"neonfields_night_p", "Neon Fields (Night)"},
-
-    {"deadeye_p", "Deadeye Canyon"},
-    { "deadeye_oasis_p", "Deadeye Canyon (Oasis)" },
-
-    { "sovereign_p", "Sovereign Heights" },
-    { "sovereign_dusk_p", "Sovereign Heights (Dusk)" },
-
-    { "estadio_p", "Estadio Vida" },
-    { "estadio_dusk_p", "Estadio Vida (Dusk)" },
-
-    { "drift_p", "Drift Woods" },
-    { "drift_dawn_p", "Drift Woods (Dawn)" },
-    { "drift_night_p", "Drift Woods (Night)" },
-
-    { "futura_p", "Futura Garden" },
-    { "futura_day_p", "Futura Garden (Day)" },
-
-    { "rivals_p", "Rivals Arena" },
-    { "rivals_day_p", "Rivals Arena (Day)" },
-
-    { "starbase_p", "Starbase ARC" },
-    { "starbase_aftermath_p", "Starbase ARC (Aftermath)" },
-
-        // Alternate Maps
-    { "throwbackstadium_p", "Throwback Stadium" },
-    { "throwbackstadium_snowy_p", "Throwback Stadium (Snowy)" },
-
-    { "underpass_p", "Underpass" },
-    { "underpass_old_p", "Underpass (Old)" },
-    { "tokyo_underpass_p", "Tokyo Underpass (Rainy)" },
-
-    { "cosmic_p", "Cosmic" },
-    { "cosmic_old_p", "Cosmic (Old)" },
-
-    { "dunkhouse_p", "Dunk House" },
-    { "dunkhouse_night_p", "Dunk House (Night)" },
-
-    { "galleon_p", "Galleon" },
-    { "galleon_retro_p", "Galleon Retro" },
-
-    { "pillars_p", "Pillars" },
-    { "octagon_p", "Octagon" },
-    { "octagon_old_p", "Octagon (Old)" },
-    { "doublegoal_p", "Double Goal" },
-    { "doublegoal_old_p", "Double Goal (Old)" },
-    { "utopia_retro_p", "Utopia Retro" },
-    { "arctagon_p", "Arctagon" },
-    { "badlands_p", "Badlands" },
-    { "badlands_night_p", "Badlands (Night)" },
-    { "barricade_p", "Barricade" },
-    { "basin_p", "Basin" },
-    { "calavera_p", "Calavera" },
-    { "carbon_p", "Carbon" },
-    { "core707_p", "Core 707" },
-    { "corridor_p", "Corridor" },
-    { "colossus_p", "Colossus" },
-    { "hourglass_p", "Hourglass" },
-    { "loophole_p", "Loophole" },
-    { "quadron_p", "Quadron" },
-    { "theblock_p", "The Block" },
-    { "theblock_dusk_p", "The Block (Dusk)" },
 
         //Manually Added Maps
     { "Underwater_P", "Aquadome" },
@@ -156,11 +27,15 @@ std::unordered_map<std::string, std::string> mapNameLookup = {
     { "Stadium_P", "DFH Stadium" },
     { "Stadium_Race_Day_p", "DFH Stadium (Circuit)" },
         //No warning for DFH Stadium Day
+    {"Stadium_day_p", "DFH Stadium (Day)"},
+
     { "Stadium_Foggy_P", "DFH Stadium (Stormy)" },
     { "woods_p", "Drift Woods" },
     { "Woods_Night_P", "Drift Woods (Night)" },
     { "FF_Dusk_P", "Estadio Vida" },
         //No warning for Farmstead
+    {"farm_p", "Farmstead"},
+
     { "Farmstead_Night_P", "Farmstead (Night)" },
     { "Farm_HW_P", "Farmstead (Spooky)" },
     { "Farm_GRS_P", "Farmstead (Pitched)" },
